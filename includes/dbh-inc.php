9e220cc3af79ab7de3775170e0f1dec977ca37e3
nior cubes <larry.mabuti.jr@gmail.com>
2022-05-07 13:31:29 +0800

    filepath1.1

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -10,19 +10,29 @@
     // if (!$conn) {
     //     die("Connection Failed" . mysqli_connect_error());
     // }
+    
+    $serverName = "us-cdbr-east-05.cleardb.net";
+    $dbUsername = "b0f3f2fd39c0c5";
+    $dbPassword = "6b40b611";
+    $dbName = "heroku_c4e7443f297f12c";
 
-
-    //Get Heroku ClearDB connection information
-    $cleardb_url = parse_url(getenv("us-cdbr-east-05.cleardb.net"));
-    $cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
-    $cleardb_username = $cleardb_url["b0f3f2fd39c0c5"];
-    $cleardb_password = $cleardb_url["6b40b611"];
-    $cleardb_db = substr($cleardb_url["path"],1);
-    $active_group = 'default';
-    $query_builder = TRUE;
-    // Connect to DB
-    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
 
     if (!$conn) {
         die("Connection Failed" . mysqli_connect_error());
     }
+
+    // //Get Heroku ClearDB connection information
+    // $cleardb_url = parse_url(getenv("us-cdbr-east-05.cleardb.net"));
+    // $cleardb_server = $cleardb_url["host"];
+    // $cleardb_username = $cleardb_url["user"];
+    // $cleardb_password = $cleardb_url["pass"];
+    // $cleardb_db = substr($cleardb_url["path"],1);
+    // $active_group = 'default';
+    // $query_builder = TRUE;
+    // // Connect to DB
+    // $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+    // if (!$conn) {
+    //     die("Connection Failed" . mysqli_connect_error());
+    // }
